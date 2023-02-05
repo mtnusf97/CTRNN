8e0d092fde5568cd11afd1f06678a993473b6fe4
Matin Yousefabadi <mtnusf97@gmail.com>
2023-02-05 17:06:48 -0500

    add job scripts

---
+++ jobs/first_job_script.py
@@ -0,0 +1,14 @@
+import pickle
+
+a = {'hello': 'world'}
+# path = '/home/mtnusf97/projects/def-cannoj9/mtnusf97/filename.pickle'
+path = 'filename.pickle'
+
+with open(path, 'wb') as handle:
+    pickle.dump(a, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('filename.pickle', 'rb') as handle:
+    b = pickle.load(handle)
+
+print(a == b)
+print('here it is')
