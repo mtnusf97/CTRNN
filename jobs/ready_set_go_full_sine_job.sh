63ed1c73d993b7c4aa9601a0286945c860d271a4
Matin Yousefabadi <mtnusf97@gmail.com>
2023-02-07 10:47:27 -0500

    update configs and jobs for multi task running

---
+++ jobs/ready_set_go_full_sine_job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --time=05:00:00
+#SBATCH --account=def-cannoj9
+#SBATCH --gpus-per-node=a100:1
+#SBATCH --ntasks-per-gpu=1
+#SBATCH --cpus-per-task=3
+#SBATCH --mem-per-cpu=2G
+
+source ../.venv/bin/activate
+echo 'starting point'
+python3 /home/mtnusf97/projects/def-cannoj9/mtnusf97/CTRNN/run_exp.py -c /home/mtnusf97/projects/def-cannoj9/mtnusf97/CTRNN/config/ready_set_go_full_sine.yaml
+echo 'Done'
+sleep 5
