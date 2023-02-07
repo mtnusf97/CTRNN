f4ac35c410c505d32386126793ac27fae8190ce7
Matin Yousefabadi <mtnusf97@gmail.com>
2023-02-07 00:28:16 -0500

    refine job directory

--- jobs/data_generate_request.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --time=00:02:00
-#SBATCH --account=def-cannoj9
-source ../.venv/bin/activate
-echo 'Hello, world!'
-python3 /home/mtnusf97/projects/def-cannoj9/mtnusf97/CTRNN/dataset_generator/gaussian_loss_derivative_generator.py
-echo 'Done'
-sleep 5
